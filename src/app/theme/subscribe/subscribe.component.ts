91515375db8d664e317a8238dec2e47f29f0f3d0
Kulwant Singh <kulwants@jcsoftwaresolution.com>
2020-02-25 12:34:29 +0530

    changes

--- src/app/theme/subscribe/subscribe.component.ts
+++ src/app/theme/subscribe/subscribe.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from '../../../environments/environment';
 import { SharedService } from '../../shared/services/shared.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-subscribe',
@@ -11,19 +13,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./subscribe.component.scss']
 })
 export class SubscribeComponent implements OnInit {
+  days: any;
+  hours: any;
+  minutes: any;
+  seconds: any;
+  private future: Date;
+  private futureString: string;
+  private message: string;
   homeForm: FormGroup;
   submitted = false;
-  _host = environment.config.BASE_URL;
+  // _host = environment.config.BASE_URL;
+  id: any;
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private _sharedService: SharedService, private toastr: ToastrService) { }
+  constructor(private router: Router,
+    private formBuilder: FormBuilder,
+    private _sharedService: SharedService,
+    private toastr: ToastrService) {
+
+  }
+
+
+
+
   get f() { return this.homeForm.controls; }
-  ngOnInit(): void {
+  ngOnInit() {
+
     this.homeForm = this.formBuilder.group({
 
       email: ['', [Validators.required, Validators.email]],
 
     });
+
+    this.timer();
+    this.id = setInterval(() => {
+      this.timer();
+    }, 1000);
+
+
   }
+
 
   submit() {
     // alert("here");
@@ -51,7 +79,24 @@ export class SubscribeComponent implements OnInit {
     });
   }
 
+  timer() {
+    var deadline = new Date('Mar 5, 2020 15:37:25').getTime();
+
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+    this.days = days;
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
+
 }
+
 
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    APP_NAME: "Wellness",
+    //local
+    // BASE_URL: "http://192.168.0.54:1338/",
+    // API_URL: "http://192.168.0.54:1338/",
+    //server
+    BASE_URL: "http://18.189.7.137:1337/",
+    API_URL: "http://18.189.7.137:1337/api/",
+  },
+  api_routes: {
+    LOGIN: 'login',
+    REGISTER: 'register',
+    MY_ACCOUNT_INFO: '',
+    EDIT_PROFILE: ''
+  }
 };

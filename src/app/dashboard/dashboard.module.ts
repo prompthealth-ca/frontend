48bcdaeec26ab9a25109baaaa2e5b94bcb90581d
Kulwant Singh <kulwants@jcsoftwaresolution.com>
2020-03-06 19:06:14 +0530

    changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SubscriptionPlanComponent } from './subscription-plan/subscription-plan.component';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [SubscriptionPlanComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgxSpinnerModule
   ]
 })
 export class DashboardModule { }

893024f1893bc361f9658a5942d5200015b4b791
mohitjc <mohitk@jcsoftwaresolution.com>
2020-03-06 13:19:45 +0530

    fdf

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,37 +1,36 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home.component";
-import { FAQComponent } from './faq/faq.component';
-import { PricvacyPolicyComponent } from './pricvacy-policy/pricvacy-policy.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
-import { LoyalityProgramsComponent } from './loyality-programs/loyality-programs.component';
-
+import { FAQComponent } from "./faq/faq.component";
+import { PricvacyPolicyComponent } from "./pricvacy-policy/pricvacy-policy.component";
+import { TermsConditionsComponent } from "./terms-conditions/terms-conditions.component";
+import { LoyalityProgramsComponent } from "./loyality-programs/loyality-programs.component";
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: "home",
     component: HomeComponent
   },
   {
-    path: 'faq',
+    path: "faq",
     component: FAQComponent
   },
   {
-    path: 'policy',
+    path: "policy",
     component: PricvacyPolicyComponent
   },
   {
-    path: 'term&conditions',
+    path: "term&conditions",
     component: TermsConditionsComponent
   },
   {
-    path: 'loyalty',
+    path: "loyalty",
     component: LoyalityProgramsComponent
-  },
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}

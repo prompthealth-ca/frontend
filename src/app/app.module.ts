6923fb99ba45a4ad1ef361a68ae94a91c471ab12
mohitjc <mohitk@jcsoftwaresolution.com>
2020-03-13 12:15:55 +0530

    fd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CookieService } from 'ngx-cookie-service';
-import { BehaviorService } from './shared/services/behavior.service';
-import { CanonicalLinkService } from './shared/services/link.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ToastrModule } from "ngx-toastr";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CookieService } from "ngx-cookie-service";
+import { BehaviorService } from "./shared/services/behavior.service";
+import { CanonicalLinkService } from "./shared/services/link.service";
 import { SocialLoginModule, AuthServiceConfig } from "angular5-social-login";
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 export function getAuthServiceConfigs() {
-  let config = new AuthServiceConfig(
-    [
-
-    ]
-  )
+  let config = new AuthServiceConfig([]);
   return config;
 }
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot()
   ],
-  providers: [BehaviorService,
+  providers: [
+    BehaviorService,
     CookieService,
     CanonicalLinkService,
     {
       provide: AuthServiceConfig,
       useFactory: getAuthServiceConfigs
-    }],
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeRoutingModule } from './home-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from '../home/home.component';
-import { FAQComponent } from './faq/faq.component';
-import { PricvacyPolicyComponent } from './pricvacy-policy/pricvacy-policy.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
-import { LoyalityProgramsComponent } from './loyality-programs/loyality-programs.component';
-import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeRoutingModule } from "./home-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from "../home/home.component";
+import { FAQComponent } from "./faq/faq.component";
+import { PricvacyPolicyComponent } from "./pricvacy-policy/pricvacy-policy.component";
+import { TermsConditionsComponent } from "./terms-conditions/terms-conditions.component";
+import { LoyalityProgramsComponent } from "./loyality-programs/loyality-programs.component";
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
-  declarations: [HomeComponent, FAQComponent, PricvacyPolicyComponent, TermsConditionsComponent, LoyalityProgramsComponent],
+  declarations: [
+    HomeComponent,
+    FAQComponent,
+    PricvacyPolicyComponent,
+    TermsConditionsComponent,
+    LoyalityProgramsComponent
+  ],
   imports: [
     NgxSpinnerModule,
     CommonModule,
     HomeRoutingModule,
     ReactiveFormsModule
-  ],
-
+  ]
 })
-export class HomeModule { }
+export class HomeModule {}

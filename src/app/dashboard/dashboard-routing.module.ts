469ebe398aca8540dec12dc56a217743302183a3
Kulwant Singh <kulwants@jcsoftwaresolution.com>
2020-03-14 16:49:12 +0530

    userdetail

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SubscriptionPlanComponent } from './subscription-plan/subscription-plan.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 const routes: Routes = [
   {
     path: "subscriptionplan",
     component: SubscriptionPlanComponent
+  },
+
+  {
+    path: "userdetails",
+    component: UserDetailsComponent
   },
 ];
 

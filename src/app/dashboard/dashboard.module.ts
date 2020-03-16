2c4d7e63ea3fe7a95d81fd681d68cda3bb6405eb
mohitjc <mohitk@jcsoftwaresolution.com>
2020-03-16 17:55:48 +0530

    design 3

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,19 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SubscriptionPlanComponent } from './subscription-plan/subscription-plan.component';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { ImageUploadModule } from 'ng2-imageupload';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { SubscriptionPlanComponent } from "./subscription-plan/subscription-plan.component";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { UserDetailsComponent } from "./user-details/user-details.component";
+
 @NgModule({
   declarations: [SubscriptionPlanComponent, UserDetailsComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    NgxSpinnerModule,
-    ImageUploadModule,
-    FormsModule
-  ]
+  imports: [CommonModule, DashboardRoutingModule, NgxSpinnerModule, FormsModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}

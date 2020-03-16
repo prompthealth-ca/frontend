78eab57c5b45ddea89c07babef353bd1822c6589
Kulwant Singh <kulwants@jcsoftwaresolution.com>
2020-03-16 17:59:59 +0530

    changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,14 +5,13 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SubscriptionPlanComponent } from './subscription-plan/subscription-plan.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { UserDetailsComponent } from './user-details/user-details.component';
-import { ImageUploadModule } from 'ng2-imageupload';
+
 @NgModule({
   declarations: [SubscriptionPlanComponent, UserDetailsComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     NgxSpinnerModule,
-    ImageUploadModule,
     FormsModule
   ]
 })

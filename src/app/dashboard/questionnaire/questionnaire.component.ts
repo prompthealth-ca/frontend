9c5b12ce81749c7f7f1b186dab3b578eede21152
TITHI AGARWAL <tithijcsoftwaresolution.com>
2020-05-01 20:22:29 +0530

    tithi_UI

--- src/app/dashboard/questionnaire/questionnaire.component.ts
+++ src/app/dashboard/questionnaire/questionnaire.component.ts
@@ -20,6 +20,88 @@ export class QuestionnaireComponent implements OnInit {
   match = false;
   subMatch = false
 
+  public subRes;
+  // public subRes1 = {
+  //   questionare: 'Option 2',
+  //   answer: [
+  //     {
+  //       id : 1,
+  //       text: "Option 1",
+  //       subans: false,
+  //     },
+  //     {
+  //       id : 2,
+  //       text: "Option 2",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 3,
+  //       text: "Option 3",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 4,
+  //       text: "Option 4",
+  //       subans: true
+  //     },
+  //   ],
+  //   choiceType: "multiple",
+  //   questionType: "Age"
+  // } 
+  // public subRes2 = {
+  //   questionare: 'Option 1',
+  //   answer: [
+  //     {
+  //       id : 1,
+  //       text: "Option 1",
+  //       subans: false,
+  //     },
+  //     {
+  //       id : 2,
+  //       text: "Option 2",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 3,
+  //       text: "Option 3",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 4,
+  //       text: "Option 4",
+  //       subans: true
+  //     },
+  //   ],
+  //   choiceType: "multiple",
+  //   questionType: "Age"
+  // }
+  // public subRes3 = {
+  //   questionare: 'Option 3',
+  //   answer: [
+  //     {
+  //       id : 1,
+  //       text: "Option 1",
+  //       subans: false,
+  //     },
+  //     {
+  //       id : 2,
+  //       text: "Option 2",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 3,
+  //       text: "Option 3",
+  //       subans: false
+  //     },
+  //     {
+  //       id : 4,
+  //       text: "Option 4",
+  //       subans: true
+  //     },
+  //   ],
+  //   choiceType: "multiple",
+  //   questionType: "Age"
+  // }
   public res = {
     success: true,
     questionare: [{
@@ -33,54 +115,17 @@ export class QuestionnaireComponent implements OnInit {
           {
             id : 2,
             text: "Option 2",
-            subans: {
-              questions: "Option 2",
-              answer:[{
-                id: 1,
-                text: "Sub Option 1",
-                subans: {
-                  questions: "Sub Option 1",
-                  answer:[
-                      {
-                          id: 1,
-                          text: "Sub Option Level Two A",
-                      },
-                      {
-                          id: 2,
-                          text: "Sub Option Level Two B",
-                      },
-                      {
-                        id: 3,
-                        text: "Sub Option Level Two C",
-                      },
-                      {
-                          id: 4,
-                          text: "Sub Option Level Two D",
-                      },
-                      {
-                        id: 5,
-                        text: "Sub Option Level Two E",
-                    },
-                    {
-                        id: 6,
-                        text: "Sub Option Level Two F",
-                    }
-                  ],
-                  choiceType: "multiple",
-                },
-              }, 
-              {
-                id: 2,
-                text: "Sub Option 2",
-                subans: false,
-              },
-              {
-                id: 3,
-                text: "Sub Option 3",
-                subans: false,
-              }],
-              choiceType: "multiple",
-            },
+            subans: true
+          },
+          {
+            id : 3,
+            text: "Option 3",
+            subans: false
+          },
+          {
+            id : 4,
+            text: "Option 4",
+            subans: false
           },
       ],
       choiceType: "multiple",
@@ -92,33 +137,12 @@ export class QuestionnaireComponent implements OnInit {
         {
           id : 1,
           text: "Option 1",
-          subans: false,
+          subans: true,
         },
         {
           id : 2,
           text: "Option 2",
-          subans: {
-            questions: "Option 2",
-            answer:[{
-              id: 1,
-              text: "Sub Option 1",
-              subans: {
-                questions: "Sub Option 1",
-                answer:[
-                  {
-                      id: 1,
-                      text: "Sub Option Level Two A",
-                  },
-                  {
-                      id: 1,
-                      text: "Sub Option Level Two B",
-                  }
-                ],
-                choiceType: "multiple",
-              }
-            }],
-          },
-          choiceType: "multiple",
+          subans: false,
         },
         {
             id : 3,
@@ -127,9 +151,34 @@ export class QuestionnaireComponent implements OnInit {
         },
 
       ],
-      choiceType: "single",
+      choiceType: "multiple",
       questionType: "Age"
-    }]
+    },
+
+    {
+      questions:"Question 3",
+      answer:[
+        {
+          id : 1,
+          text: "Option 1",
+          subans: false,
+        },
+        {
+          id : 2,
+          text: "Option 2",
+          subans: false,
+        },
+        {
+            id : 3,
+            text: "Option 3",
+            subans: true,
+        },
+
+      ],
+      choiceType: "multiple",
+      questionType: "Age"
+    }
+  ]
   };
 
   constructor
@@ -154,32 +203,92 @@ export class QuestionnaireComponent implements OnInit {
     else {
       this.type = 'U';
     }
-    // let path = `questionare?type=${this.type}`;
-    // this._sharedService.get(path).subscribe((res: any) => {
-    //   // this._sharedService.loader('hide');
-      // if (this.res.success) {
-      //   this.questionnaire = this.res.questionare;
+    let path = `questionare?type=${this.type}`;
+    this._sharedService.get(path).subscribe((res: any) => {
+      // this._sharedService.loader('hide');
+
+      console.log('this.questionnaire-------', res.questionare);
+      if (res.success) {
+        this.questionnaire = res.questionare;
         
-      // } else {
-      //   this._sharedService.checkAccessToken(res.error);
-      // }
-    // }, err => {
+      } else {
+        this._sharedService.checkAccessToken(res.error);
+      }
+    }, err => {
 
-    //   this._sharedService.checkAccessToken(err);
-    // });
-    if (this.res.success) {
-      this.questionnaire = this.res.questionare;
-
-      console.log('this.questionnaire', this.questionnaire);
-      
-    } 
-   
+      this._sharedService.checkAccessToken(err);
+    });
   }
 
   saveQuestionnaire() {
     // TODO: Call the API to save questions
     console.log('type comes here', this.type);
     this.type === 'U' ? this._router.navigate(['/dashboard/listing']) : this._router.navigate(['/dashboard/professional']);
+
+  }
+  getSubAns(evt) {
+  //   setTimeout(()=>{
+  //     if(evt.target.name === 'Option 1') {
+  //       this.subRes = this.subRes2
+  //     }
+  //     if(evt.target.name === 'Option 2') {
+  //       this.subRes = this.subRes1
+  //     }
+  //     if(evt.target.name === 'Option 3') {
+  //       this.subRes = this.subRes3
+  //     }
+  //     this.subRes = {
+  //       questions: "Option 2",
+  //       answer:[{
+  //         id: 1,
+  //         text: "Sub Option 1",
+  //         subans: {
+  //           questions: "Sub Option 1",
+  //           answer:[
+  //               {
+  //                   id: 1,
+  //                   text: "Sub Option Level Two A",
+  //               },
+  //               {
+  //                   id: 2,
+  //                   text: "Sub Option Level Two B",
+  //               },
+  //               {
+  //                 id: 3,
+  //                 text: "Sub Option Level Two C",
+  //               },
+  //               {
+  //                   id: 4,
+  //                   text: "Sub Option Level Two D",
+  //               },
+  //               {
+  //                 id: 5,
+  //                 text: "Sub Option Level Two E",
+  //             },
+  //             {
+  //                 id: 6,
+  //                 text: "Sub Option Level Two F",
+  //             }
+  //           ],
+  //           choiceType: "multiple",
+  //         },
+  //       }, 
+  //       {
+  //         id: 2,
+  //         text: "Sub Option 2",
+  //         subans: false,
+  //       },
+  //       {
+  //         id: 3,
+  //         text: "Sub Option 3",
+  //         subans: false,
+  //       }],
+  //       choiceType: "multiple",
+  //     }
+  //   }, 3000);
+
+
+  //   console.log('subRes......', this.subRes)
 
   }
 }

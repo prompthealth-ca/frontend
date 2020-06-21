31365399ebeb973fd0c4cabeb4b1d0d47042af2e
TITHI AGARWAL <tithijcsoftwaresolution.com>
2020-06-21 12:16:36 +0530

    fix done

--- src/app/home/affiliate/affiliate.component.ts
+++ src/app/home/affiliate/affiliate.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { SharedService } from '../../shared/services/shared.service';
 
 @Component({
   selector: 'app-affiliate',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AffiliateComponent implements OnInit {
 
-  constructor() { }
+  affiliateRequestForm: FormGroup;
+  submitted = false;
+  email: any;
+  constructor(
+    private formBuilder: FormBuilder,
+    private _sharedService: SharedService,
+    private toastr: ToastrService,
+  ) { }
+  get f() { return this.affiliateRequestForm.controls; }
 
   ngOnInit(): void {
+    this.affiliateRequestForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', []],
+      email: ['', [Validators.required, Validators.email]],
+      numberOfUser: ['', [Validators.required]],
+      role: ['SP', [Validators.required]],
+      message: ['',],
+    });
+  }
+  
+  affiliateMe() {
+    console.log('affiliateMe', this.affiliateRequestForm)
+    this.submitted = true;
+    if (this.affiliateRequestForm.invalid) {
+      return;
+    }
+    else {
+      this.submitted = true;
+      let data = this.affiliateRequestForm.value;
+      data.isVipAffiliateUser = true;
+      console.log('data', data);
+      this._sharedService.loader('show');
+      let path = 'user/request'
+      this._sharedService.postNoAuth(data, path).subscribe((res: any) => {
+        this._sharedService.loader('hide');
+        if (res.statusCode === 200) {
+          this.toastr.success(res.message);
+          this.affiliateRequestForm.reset();
+          this.submitted = false;
+
+        }
+
+        else {
+          this.toastr.error(res.message);
+        }
+      }, (error) => {
+        this._sharedService.loader('hide');
+      });
+    }
+
   }
 
 }
--- src/app/theme/footer/footer.component.ts
+++ src/app/theme/footer/footer.component.ts
@@ -11,10 +11,6 @@ declare var FB: any;
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  
-  @ViewChild('closebutton') closebutton;
-  affiliateRequestForm: FormGroup;
-  submitted = false;
   email: any;
   constructor(
     private formBuilder: FormBuilder,
@@ -23,16 +19,8 @@ export class FooterComponent implements OnInit {
     private toastr: ToastrService,
   ) { }
 
-    get f() { return this.affiliateRequestForm.controls; }
+  
   ngOnInit() {
-    this.affiliateRequestForm = this.formBuilder.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', []],
-      email: ['', [Validators.required, Validators.email]],
-      numberOfUser: ['', [Validators.required]],
-      role: ['SP', [Validators.required]],
-      message: ['', [Validators.required]],
-    });
   }
 
   route(path, queryParams) {
@@ -55,37 +43,6 @@ export class FooterComponent implements OnInit {
         this._sharedService.showAlert(res.error.message, 'alert-danger');
       }
     }, (error) => { });
-  }
-  affiliateMe() {
-    this.submitted = true;
-    if (this.affiliateRequestForm.invalid) {
-      return;
-    }
-    else {
-      this.submitted = true;
-      let data = this.affiliateRequestForm.value;
-      data.isVipAffiliateUser = true;
-      console.log('data', data);
-      this._sharedService.loader('show');
-      let path = 'user/request'
-      this._sharedService.postNoAuth(data, path).subscribe((res: any) => {
-        this._sharedService.loader('hide');
-        if (res.statusCode === 200) {
-          this.toastr.success(res.message);
-          this.closebutton.nativeElement.click();
-          this.affiliateRequestForm.reset();
-          this.submitted = false;
-
-        }
-
-        else {
-          this._sharedService.showAlert(res.message, 'alert-danger');
-        }
-      }, (error) => {
-        this._sharedService.loader('hide');
-      });
-    }
-
   }
 
 }

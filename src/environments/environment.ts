1fbec5218a33706fa0752c69c8cfe58fb1b0a319
TITHI AGARWAL <tithijcsoftwaresolution.com>
2020-07-23 23:53:02 +0530

    API url  updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,8 @@ export const environment = {
     // API_URL: "http://3.12.81.245:1337/"
 
     BASE_URL: "http://3.12.81.245:3000/api/v1/",
-    API_URL: "http://3.12.81.245:3000/",
+    // API_URL: "http://3.12.81.245:3000/",
+    API_URL: "https://prompthealth.ca:3000/"
   },
   api_routes: {
     LOGIN: 'login',

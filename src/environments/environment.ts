7135e3d2aec842638ed712ecda860c085de87fe2
TITHI AGARWAL <tithijcsoftwaresolution.com>
2020-09-01 22:06:22 +0530

    api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   config: {
     APP_NAME: "Wellness",
 
-    BASE_URL: "https://prompthealth.ca:3000/api/v1/",
-    API_URL: "https://prompthealth.ca:4200/api/v1/",
+    BASE_URL: "https://prompthealth.ca/",
+    API_URL: "https://prompthealth.ca:3000/api/v1/",
 
     // BASE_URL: "https://prompthealth.ca:4200/",
     // API_URL: "https://prompthealth.ca:3000/",

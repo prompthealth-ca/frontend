e963127bf3f91c7e6d618ee770a9d4fd8d584e0c
Ranjeet <ranjeets@smartdatainc.net>
2020-12-01 00:44:00 +0530

    local env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,9 @@ export const environment = {
     // BASE_URL: "https://prompthealth.ca/",
     // API_URL: "https://prompthealth.ca:3000/api/v1/",
 
-    BASE_URL: "http://localhost:4200/",
-    API_URL: "https://c79aeae3034a.ngrok.io/api/v1/",
+   BASE_URL: "http://localhost:4200/",
+    // API_URL: "https://c79aeae3034a.ngrok.io/api/v1/",
+    API_URL: "http://localhost:3000/api/v1/",
   },
   api_routes: {
     LOGIN: 'login',

541b714b9e5b20911167d677ef6c23f4e11e05a4
Ranjeet <ranjeets@smartdatainc.net>
2020-12-01 23:55:53 +0530

    STAGING CODE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,11 @@ export const environment = {
   config: {
     APP_NAME: "Wellness",
     //server
-    BASE_URL: "http://3.12.81.245:3000/api/v1/",
-    API_URL: "http://3.12.81.245:3000/api/v1/",
+    // BASE_URL: "http://3.12.81.245:3000/api/v1/",
+    // API_URL: "http://3.12.81.245:3000/api/v1/",
 
+    BASE_URL: "http://198.251.65.146:4202/api/v1",
+    API_URL: "http://198.251.65.146:4202/api/v1",
 
     // BASE_URL: "https://prompthealth.ca/",
     // API_URL: "https://prompthealth.ca:3000/api/v1/",

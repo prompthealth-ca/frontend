d3b9f14c8e1a73f072bc6bba2acaeb448efa16e4
Otto Hu <ottohu101@gmail.com>
2020-12-02 12:11:26 -0800

    home page rework WIP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, ElementRef, ViewChild, NgZone } from "@angular/core";
-import { ToastrService } from "ngx-toastr";
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { MapsAPILoader, MouseEvent } from '@agm/core';
-import { Router } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { SharedService } from "../shared/services/shared.service";
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedService } from '../shared/services/shared.service';
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   @ViewChild('searchGlobal')
   public searchGlobalElementRef: ElementRef;
 
-  token = "";
+  token = '';
   days: any;
   hours: any;
   minutes: any;
@@ -24,39 +24,40 @@ export class HomeComponent implements OnInit {
   private geoCoder;
   homeForm: FormGroup;
   submitted = false;
-  roles = ''
+  roles = '';
   zipCodeSearched;
   lat;
   long;
   // _host = environment.config.BASE_URL;
   id: any;
-  showPersonalMatch:boolean = true;
+  showPersonalMatch = true;
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
+    // tslint:disable-next-line: variable-name
     private _sharedService: SharedService,
     private toastr: ToastrService,
     private mapsAPILoader: MapsAPILoader,
     private ngZone: NgZone,
-  ) {}
+  ) { }
 
   get f() {
     return this.homeForm.controls;
   }
   ngOnInit() {
-    
-    this.roles = localStorage.getItem('roles') ? localStorage.getItem('roles') : ''
+
+    this.roles = localStorage.getItem('roles') ? localStorage.getItem('roles') : '';
     localStorage.removeItem('searchedAddress');
-    this.token = localStorage.getItem("token");
-    if(this.token){
-      if(this.roles=='SP'|| this.roles=='C'){
+    this.token = localStorage.getItem('token');
+    if (this.token) {
+      if (this.roles === 'SP' || this.roles === 'C') {
         this.showPersonalMatch = false;
-      }else{
+      } else {
         this.showPersonalMatch = true;
       }
     }
     this.homeForm = this.formBuilder.group({
-      email: ["", [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]]
     });
 
     this.timer();
@@ -66,25 +67,25 @@ export class HomeComponent implements OnInit {
   }
 
   findDoctor() {
-    this.lat = 0 + localStorage.getItem("ipLat");
-    this.long = 0 + localStorage.getItem("ipLong");
-    this.router.navigate(['/doctor-filter'], { queryParams: {lat: this.lat, long: this.long}});
+    this.lat = 0 + localStorage.getItem('ipLat');
+    this.long = 0 + localStorage.getItem('ipLong');
+    this.router.navigate(['/doctor-filter'], { queryParams: { lat: this.lat, long: this.long } });
   }
   questionnaire() {
     if (this.token) {
       this.router.navigate(['dashboard/questions/User']);
     } else {
-      this.router.navigate(['dashboard/questions/User'])
+      this.router.navigate(['dashboard/questions/User']);
       // this.router.navigate(['auth/login/u']);
       // this.toastr.warning("Please login first.")
-    } 
+    }
   }
 
   submit() {
     // alert("here");
 
     this.submitted = true;
-    let data = JSON.stringify(this.homeForm.value);
+    const data = JSON.stringify(this.homeForm.value);
 
     // this._sharedService.loader('show');
     this._sharedService.contactus(data).subscribe(
@@ -99,21 +100,21 @@ export class HomeComponent implements OnInit {
         }
       },
       error => {
-        this.toastr.error("Please check your email id.");
-        //this._sharedService.loader('hide');
+        this.toastr.error('Please check your email id.');
+        // this._sharedService.loader('hide');
       }
     );
   }
 
   timer() {
-    var deadline = new Date("June 5, 2020 15:37:25").getTime();
+    const deadline = new Date('June 5, 2020 15:37:25').getTime();
 
-    var now = new Date().getTime();
-    var t = deadline - now;
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    const now = new Date().getTime();
+    const t = deadline - now;
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((t % (1000 * 60)) / 1000);
 
     this.days = days;
     this.hours = hours;
@@ -121,10 +122,10 @@ export class HomeComponent implements OnInit {
     this.seconds = seconds;
   }
 
-  learnMore(){
-    if(this.token){
+  learnMore() {
+    if (this.token) {
       this.router.navigate(['dashboard/subscriptionplan']);
-    }else{
+    } else {
       this.router.navigate(['subscriptionplan']);
     }
   }

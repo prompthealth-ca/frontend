db9697e088ac2704a18d3e7c4a8b73d8621cc454
Ranjeet <ranjeets@smartdatainc.net>
2020-12-08 23:26:06 +0530

    Stripe test key changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -53,7 +53,7 @@ import { UserQuestionaireComponent } from './user-questionaire/user-questionaire
     FormsModule,
     ReactiveFormsModule,
     // NgxStripeModule.forRoot('pk_live_51HMSVQHzvKsIv7FclCIgEYNrD4tlvjzZRTDx43Y54pVY3vjQ8MhFuOntQMY094MZ49bDzOdFf2A2tkYdTwSag9ij00xDUu4xnU'),
-    NgxStripeModule.forRoot('pk_test_zqD7pwcCCzFTnYdL8NhZeIl600rcJJW5dU'),
+    NgxStripeModule.forRoot('pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'),
     NgMultiSelectDropDownModule.forRoot(),
     UiSwitchModule,
   ]

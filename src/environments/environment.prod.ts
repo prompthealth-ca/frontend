de4605426cdd55a2ebfb19730d5f3efb56acc754
Ubuntu <ubuntu@ip-172-31-15-155.us-east-2.compute.internal>
2020-12-10 23:43:49 +0000

    staging use test api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
 
     BASE_URL: 'https://prompthealth.ca/',
     API_URL: 'https://prompthealth.ca:3000/api/v1/',
-    stripeKey: 'pk_live_51HMSVQHzvKsIv7FclCIgEYNrD4tlvjzZRTDx43Y54pVY3vjQ8MhFuOntQMY094MZ49bDzOdFf2A2tkYdTwSag9ij00xDUu4xnU'
+    stripeKey: 'pk_test_zqD7pwcCCzFTnYdL8NhZeIl600rcJJW5dU'
   },
   api_routes: {
     LOGIN: 'login',

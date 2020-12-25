143456bf9bbf303a370c47cbe6cdf1874237d416
Otto Hu <ottohu101@gmail.com>
2020-12-24 16:58:25 -0800

    prepare for staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,11 +10,11 @@ export const environment = {
     //  BASE_URL: "https://prompthealth.ca/",
     //  API_URL: "https://prompthealth.ca:3000/api/v1/",
 
-    // BASE_URL: 'http://198.251.65.146:4200/',
-    // API_URL: 'http://198.251.65.146:4202/api/v1/',
+    BASE_URL: 'http://198.251.65.146:4200/',
+    API_URL: 'http://198.251.65.146:4202/api/v1/',
 
-    BASE_URL: 'http://localhost:3000/',
-    API_URL: 'http://localhost:3000/api/v1/',
+    // BASE_URL: 'http://localhost:3000/',
+    // API_URL: 'http://localhost:3000/api/v1/',
     //   // API_URL: "https://c79aeae3034a.ngrok.io/api/v1/",
     stripeKey: 'pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'
   },

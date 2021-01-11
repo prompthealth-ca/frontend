3091458d9fc39d901035b0a21836cd804576e81a
Ranjeet <ranjeets@smartdatainc.net>
2021-01-11 21:43:48 +0530

    staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
     // stripeKey: 'pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'
 
     //Local host
-    // BASE_URL: "http://localhost:3000",
-    // API_URL: "http://localhost:3000/api/v1/",
+    // BASE_URL: "https://localhost:3000",
+    // API_URL: "https://localhost:3000/api/v1/",
     // stripeKey: 'pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'
     
 

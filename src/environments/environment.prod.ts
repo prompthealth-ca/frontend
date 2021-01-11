662a35121b4319906dbfd28b2e73a2bbcbbf96ac
Ranjeet <ranjeets@smartdatainc.net>
2021-01-11 22:26:32 +0530

    stagaing code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,10 +11,10 @@ export const environment = {
     // BASE_URL: "https://localhost:3000",
     // API_URL: "https://localhost:3000/api/v1/",
     // stripeKey: 'pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'
-    
+      
 
     // //Staging
-    BASE_URL: "https://prompthealth.ca:3000",
+    BASE_URL: "https://prompthealth.ca:4200",
     API_URL: "https://prompthealth.ca:3000/api/v1/",
     stripeKey: 'pk_test_51HMSVQHzvKsIv7FcySpZJiaqJEpFyeV4T1fqzmTaIMKAt8VoIcSFNOoy0xChuIec3fotWjF00FAMMNI5MZRvr10X00NqhMqjLR'
     

ab68c4a84f774abeb8df26a0353578ab740622a6
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-22 10:24:16 -0800

    enable scroll down to addon section

--- src/app/scrolltop.service.ts
+++ src/app/scrolltop.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivationStart } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
@@ -11,10 +11,23 @@ export class ScrollTopService {
     private router: Router) {
   }
 
+  private isInitial: boolean = true; 
+
   setScrollTop() {
     if (isPlatformBrowser(this.platformId)) {
       this.router.events.subscribe((event: NavigationEnd) => {
-        window.scroll(0, 0);
+        if(event instanceof ActivationStart){ this.isInitial = false; }
+        
+        if(event instanceof NavigationEnd && event.url.match(/#addon/)){
+          var timer = this.isInitial? 1000 : 300;
+          var el = document.querySelector('#addon');
+          
+          setTimeout(()=>{
+            window.scroll(0, el.getBoundingClientRect().top - 100)
+          }, timer)
+
+        }
+        else{ window.scroll(0, 0); }
       });
     }
   }

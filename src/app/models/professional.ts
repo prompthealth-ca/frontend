c2aa78486a61e8283a23eb1eda43fc647daa8a3d
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-01-25 21:01:53 -0800

    some changes

--- src/app/models/professional.ts
+++ src/app/models/professional.ts
@@ -12,12 +12,12 @@ export interface IProfessional {
   address: string;
   rating: string | number;
   isCentre: boolean;
+  provideVirtual: boolean;
 
   //used in listingComponent
   price: string;
   location: number[]; // geo location
   distance: number;
-  provideVirtual: boolean;
   mapLabel: string;
   isCheckedForCompare: boolean;
 
@@ -32,6 +32,12 @@ export interface IProfessional {
   ageRange: string;
   organization: string;
   certification: string;
+
+  treatmentModality: string[];
+  service: string[];
+  serviceDelivery: string[]
+  amenity: string[];
+  typeOfProvider: string[];
 
   //not used anywhere yet
   gender: string;
@@ -66,6 +72,7 @@ export class Professional implements IProfessional{
   private _firstname: string;
   private _image: string;
   private _roles: string[];
+  private _description: string;
   private _phone: string;
   private _ratingAvg: number;
   private _reviews: any[] = [];
@@ -80,7 +87,8 @@ export class Professional implements IProfessional{
   private _practicePhilosophy: string;
   private _organization: string;
   private _certification: string;
-  private _professionals: Professional[];
+  private _professionals: Professional[] = [];
+  private _amenities: Amenity[];
 
   private _isCheckedForCompared: boolean = false;;
 
@@ -100,18 +108,19 @@ export class Professional implements IProfessional{
 
   private _banner: string //todo: implement correctly. currently, this property not used.
   private _endosements: any[] //todo: impliment correctly. currently, this property not used;
-
+  
   get id(){ return this._id; }
   get name(){ return this._name; }
   get firstname(){ return this._firstname; }
   get image(){ return this._image; }
   get banner(){ return this._banner; }
   get role(){ return this._roles.toString(); }
+  get description(){ return this._description; }
   get phone(){ return this._phone; }
   get reviews(){ return this._reviews; }
   get rating(){ return this._ratingAvg; }
   get price(){ return (this._priceRange.length>1)? `$${this._priceRange[0]}+ / hr` : (this._priceRange.length == 1)? `$${this._priceRange[0]} / hr` : null; }
-  get priceFull(){ return '$' + this._priceRange.join(' - ')}
+  get priceFull(){ return (this._priceRange.length == 0)? 'N/A' : '$' + this._priceRange.join(' - ')}
   get gender(){ return this._gender; }
   get address(){ return this._address; }
   get location(){ return this._location; }
@@ -143,33 +152,33 @@ export class Professional implements IProfessional{
   get professionals(){ return this._professionals; }
 
   get typeOfProvider(){
-    if(this._typeOfProvider.length == 0){ return 'N/A'; }
-
-    var types = [];
-    this._typeOfProvider.forEach(type=>{ types.push(type.item_text); });
-    return types.join(', ');
+    var result = [];
+    if(this._typeOfProvider){ this._typeOfProvider.forEach(o=>{ result.push(o.item_text); }); }
+    return result;
   }
 
   get treatmentModality(){
-    if(this._treatmentModality.length == 0){ return 'N/A'; }
-
-    var modalities = [];
-    this._treatmentModality.forEach(modality=>{ modalities.push(modality.item_text); });
-    return modalities.join(', ');
+    var result = [];
+    if(this._treatmentModality){ this._treatmentModality.forEach(o=>{ result.push(o.item_text); }); }
+    return result;
   }
 
   get service(){
-    if(this._service.length == 0){ return 'N/A'; }
-    var services = [];
-    this._service.forEach(s=>{ services.push(s.item_text); })
-    return services.join(', ');
+    var result = [];
+    if(this._service){ this._service.forEach(o=>{ result.push(o.item_text); }); }
+    return result; 
   }
 
   get serviceDelivery(){
-    if(this._serviceDelivery.length == 0){ return 'N/A'; }
-    var services = [];
-    this._serviceDelivery.forEach(s=>{ services.push(s.item_text); })
-    return services.join(', ');
+    var result = [];
+    if(this._serviceDelivery){ this._serviceDelivery.forEach(o=>{ result.push(o.item_text); }); }
+    return result;
+  }
+
+  get amenity(){
+    var result = [];
+    if(this._amenities){ this._amenities.forEach(o=>{ result.push(o.item_text); }); }
+    return result;
   }
 
   get isCheckedForCompare(){ return this._isCheckedForCompared; }
@@ -182,18 +191,20 @@ export class Professional implements IProfessional{
     this._rowUserData = p;
     this._rowAns = ans;
 
-    var name = [];
-    if(p.firstName && p.firstName.length > 0){ name.push(p.firstName); }
-    if(p.lastName && p.lastName.length > 0){ name.push(p.lastName); }
-    this._name = name.join(' ');
+    var first = p.firstName || p.fname || '';
+    var last = p.lastName || p.lname || '';
+    var name = first + ' ' + last;
+    this._name = name.trim();
+    this._firstname = first;
 
-    this._firstname = p.firstName;
-
-    this._image = (p.profileImage && p.profileImage.length > 0)? baseURLImage + p.profileImage : '/assets/img/no-image.jpg';
+    var image = p.profileImage || p.image || null;  
+    this._image = (image && image.length > 0)? baseURLImage + image : '/assets/img/no-image.jpg';
     this._banner = '/assets/img/professional-banner.png';
 
-    this._roles = (typeof p.roles == 'string')? [p.roles] : p.roles;
-    this._practicePhilosophy = p.product_description;
+    this._roles = !p.roles? ['SP'] : (typeof p.roles == 'string')? [p.roles] : p.roles;
+
+    this._description = p.description || null;
+    this._practicePhilosophy = p.product_description || null;
 
     var phone: string;
     if(!p.phone){ phone = null; }
@@ -202,30 +213,31 @@ export class Professional implements IProfessional{
     else { phone = p.phone; }
     this._phone = phone;
 
-    this._ratingAvg = Number(p.ratingAvg);
+    this._ratingAvg = p.ratingAvg? Number(p.ratingAvg) : 0;
     this._reviews = p.ratingBy || [];
     
-    var priceRange: string[] = p.price_per_hours.replace('$', '').split('-');
-    priceRange.forEach((price,i)=>{ 
+    var priceRange: string = p.price_per_hours || '';
+    var priceArray: string[] = priceRange? priceRange.replace('$', '').split('-') : [];
+    priceArray.forEach((price)=>{ 
       var p = Number(price.trim());
       if(p > 0){ this._priceRange.push(p); }
-    })
+    });
 
-    this._gender = (p.gender && p.gender.length>0)? p.gender : 'Not Mentioned';
-    this._address = p.address;
-    this._videos = p.videos;
-    this._yearsOfExperience = p.years_of_experience;
+    this._gender = p.gender || null;
+    this._address = p.address || null;
+    this._videos = p.videos || [];
+    this._yearsOfExperience = p.years_of_experience || null;
     this._languagesId = p.languages || [];
     this._availabilityId = p.typical_hours || [];
     this._ageRangeId = p.age_range || [];
+    this._ageRange = p.age_range || [];
     this._serviceDeliveryId = p.serviceOfferIds || [];
-    this._location = p.location;
-    this._distance = p.calcDistance;
-    this._provideVirtual = p.provideVirtual;
+    this._location = p.location || null;
+    this._distance = p.calcDistance || null;
+    this._provideVirtual = p.provideVirtual || false;
 
-    this._ageRange = p.age_range;
-    this._organization = p.professional_organization;
-    this._certification = p.certification;
+    this._organization = p.professional_organization || null;
+    this._certification = p.certification || null;
   }
 
   populate(type: 'languages' | 'availability' | 'ageRange' | 'serviceDelivery', dataSet: QuestionnaireAnswer[]){
@@ -243,14 +255,26 @@ export class Professional implements IProfessional{
 
   setServiceCategory(name: string, data: ServiceCategory[]){ this['_' + name] = data; }
   setEndosements(endosements: any[]){ this._endosements = endosements; }
+  setProfessionals(professionals: Professional[]){  professionals.forEach(p=>{ this._professionals.push(p); }); }
 
+  setAmenities(amenities: any[]){
+    var a = [];
+    amenities.forEach(amenity => {
+      a.push({
+        id: amenity.defaultamenityId._id, 
+        item_text: amenity.defaultamenityId.item_text,
+        image: (amenity.defaultamenityId.icon? ('/assets/img/' + amenity.defaultamenityId.icon) : null), 
+      });
+    });
+    this._amenities = a;
+  }
+  setReviews(reviews: any[]){ this._reviews = reviews || []; }
   sortReviewBy(i: number){
     switch(i){
       case 0: this._reviews.sort((a,b)=> b.rate - a.rate ); break; /** rate desc */
       case 1: this._reviews.sort((a,b)=> a.rate - b.rate ); break; /** rate asc */
       case 2: this._reviews.sort((a,b)=> b.updatedAt.getTime() - a.updatedAt.getTime() ); break; /** date desc */
     }
-
   }
 }
 
@@ -267,4 +291,10 @@ type Video = {
   _id: string,
   title: string,
   url: string;
+}
+
+type Amenity = {
+  id: string
+  item_text: string;
+  image: string;
 }

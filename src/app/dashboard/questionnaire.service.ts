734f735614cdbf0906858a20e2a3fde4d97e0c76
Otto Hu <ottohu101@gmail.com>
2021-02-09 18:54:55 -0800

    format

--- src/app/dashboard/questionnaire.service.ts
+++ src/app/dashboard/questionnaire.service.ts
@@ -6,105 +6,103 @@ import { Subject, Observable } from 'rxjs';
 })
 export class QuestionnaireService {
 
-  public  observeQuestionnaire(): Observable<any>{ return this.questionnaireObserver; }
+
+  constructor() { }
   private questionnaireObserver = new Subject<any>();
-  private emitQuestionnaire(){ this.questionnaireObserver.next(this.getQuestionnaire()); }
-  private throwQuestionnaire(i: number, status: string){ this.questionnaireObserver.error({idxBackTo: i, backTo:this.questionnaireTypes[i].label , status: status}); }
-
-  public  observeCurrentQuestionnaireType(): Observable<[string, number]>{ return this.currentQuestionnaireTypeObserver; }
   private currentQuestionnaireTypeObserver = new Subject<[string, number]>();
-  private emitCurrentQuestionnaireType(){
-    this.currentQuestionnaireTypeObserver.next([this.currentQuestionnaireType, this.getCurrentIdxQuestionnaire()]); 
-  }
-
-  public  observeSubmit(): Observable<void>{ return this.submitObserver; };
   private submitObserver = new Subject<void>();
-  public  emitSubmit(){ this.submitObserver.next(); }
 
   private currentQuestionnaireType: string;
   private questionnaire: Questionnaire[];
 
   private questionnaireTypes: QuestionnaireType[] = [
-    {id: 'age',          label: 'Age',                slug: 'age-range-of-clients',   personalMatchTarget: 'age_range'},
-    {id: 'background',   label: 'Health Background',  slug: 'who-are-your-customers',  personalMatchTarget: 'ids'},
-    {id: 'goal',         label: 'Goal',               slug: 'your-goal-specialties',  personalMatchTarget: 'ids'},
-    {id: 'availability', label: 'Availability',       slug: 'typical-hours',          personalMatchTarget: 'typical_hours'},
-  ]
+    { id: 'age', label: 'Age', slug: 'age-range-of-clients', personalMatchTarget: 'age_range' },
+    { id: 'background', label: 'Health Background', slug: 'who-are-your-customers', personalMatchTarget: 'ids' },
+    { id: 'goal', label: 'Goal', slug: 'your-goal-specialties', personalMatchTarget: 'ids' },
+    { id: 'availability', label: 'Availability', slug: 'typical-hours', personalMatchTarget: 'typical_hours' },
+  ];
 
-  constructor() { }
+  public observeQuestionnaire(): Observable<any> { return this.questionnaireObserver; }
+  private emitQuestionnaire() { this.questionnaireObserver.next(this.getQuestionnaire()); }
+  private throwQuestionnaire(i: number, status: string) { this.questionnaireObserver.error({ idxBackTo: i, backTo: this.questionnaireTypes[i].label, status }); }
 
-  isFirstQuestion(){ return (this.getCurrentIdxQuestionnaire() == 0); }
-  isLastQuestion(){ return (this.getCurrentIdxQuestionnaire() == this.questionnaireTypes.length - 1);}
+  public observeCurrentQuestionnaireType(): Observable<[string, number]> { return this.currentQuestionnaireTypeObserver; }
+  private emitCurrentQuestionnaireType() {
+    this.currentQuestionnaireTypeObserver.next([this.currentQuestionnaireType, this.getCurrentIdxQuestionnaire()]);
+  }
 
-  getQuestionnaireTypes(){ return this.questionnaireTypes; }
+  public observeSubmit(): Observable<void> { return this.submitObserver; } public emitSubmit() { this.submitObserver.next(); }
 
-  setCurrentQuestionnaireType(type: string){
+  isFirstQuestion() { return (this.getCurrentIdxQuestionnaire() === 0); }
+  isLastQuestion() { return (this.getCurrentIdxQuestionnaire() === this.questionnaireTypes.length - 1); }
+
+  getQuestionnaireTypes() { return this.questionnaireTypes; }
+
+  setCurrentQuestionnaireType(type: string) {
     this.currentQuestionnaireType = type;
     this.emitCurrentQuestionnaireType();
   }
 
-  getQuestionnaire(type?: string): Questionnaire{ 
-    if(!this.questionnaire || this.questionnaire.length == 0 || !this.currentQuestionnaireType){ return null; }
-    if(!type){ type = this.currentQuestionnaireType; }
+  getQuestionnaire(type?: string): Questionnaire {
+    if (!this.questionnaire || this.questionnaire.length === 0 || !this.currentQuestionnaireType) { return null; }
+    if (!type) { type = this.currentQuestionnaireType; }
 
-    var qType: QuestionnaireType;
-    for(i=0; i<this.questionnaireTypes.length; i++){
-      if(this.questionnaireTypes[i].id == type){ qType = this.questionnaireTypes[i]; break;}
+    let qType: QuestionnaireType;
+    for (let i = 0; i < this.questionnaireTypes.length; i++) {
+      if (this.questionnaireTypes[i].id === type) { qType = this.questionnaireTypes[i]; break; }
     }
 
-    var q: any = null;
-    for(var i=0; i<this.questionnaire.length; i++){
-      if(this.questionnaire[i].slug == qType.slug){ q = this.questionnaire[i]; break;}
+    let q: any = null;
+    for (let i = 0; i < this.questionnaire.length; i++) {
+      if (this.questionnaire[i].slug === qType.slug) { q = this.questionnaire[i]; break; }
     }
     return q;
   }
-  getQuestionnaireAll(){ return this.questionnaire; }
+  getQuestionnaireAll() { return this.questionnaire; }
 
-  setQuestionnaire(q: any[], emit: boolean = true){ 
+  setQuestionnaire(q: any[], emit: boolean = true) {
     this.questionnaire = q;
 
-    var idxCurrent = this.getCurrentIdxQuestionnaire();
-    if(idxCurrent > 0){
-      for(var i=0; i<idxCurrent; i++){
-        var isAnswered = false;
-        this.questionnaire[i].answers.forEach(a=>{
-          if(a.active){ isAnswered = true; }
+    const idxCurrent = this.getCurrentIdxQuestionnaire();
+    if (idxCurrent > 0) {
+      for (let i = 0; i < idxCurrent; i++) {
+        let isAnswered = false;
+        this.questionnaire[i].answers.forEach(a => {
+          if (a.active) { isAnswered = true; }
         });
-        if(!isAnswered){ 
+        if (!isAnswered) {
           this.throwQuestionnaire(i, 'not answered some question before');
           return;
         }
       }
     }
-    if(emit){ this.emitQuestionnaire(); }
+    if (emit) { this.emitQuestionnaire(); }
   }
 
-  getCurrentIdxQuestionnaire(){
-    var idxCurrentQuestionnaire: number = null;
-    for(var i=0; i<this.questionnaireTypes.length; i++){
-      if(this.questionnaireTypes[i].id == this.currentQuestionnaireType){
+  getCurrentIdxQuestionnaire() {
+    let idxCurrentQuestionnaire: number = null;
+    for (let i = 0; i < this.questionnaireTypes.length; i++) {
+      if (this.questionnaireTypes[i].id === this.currentQuestionnaireType) {
         idxCurrentQuestionnaire = i;
         break;
       }
     }
     return idxCurrentQuestionnaire;
   }
-  getQuestionnaireTypePre(){
-    var i = this.getCurrentIdxQuestionnaire();
-    if(i == 0){ return null; }
-    else{ return this.questionnaireTypes[i - 1].id; }
+  getQuestionnaireTypePre() {
+    const i = this.getCurrentIdxQuestionnaire();
+    if (i === 0) { return null; } else { return this.questionnaireTypes[i - 1].id; }
   }
-  getQuestionnaireTypeNext(){
-    var i = this.getCurrentIdxQuestionnaire();
-    if(i >= this.questionnaireTypes.length - 1){ return null; }
-    else{ return this.questionnaireTypes[i + 1].id; }
+  getQuestionnaireTypeNext() {
+    const i = this.getCurrentIdxQuestionnaire();
+    if (i >= this.questionnaireTypes.length - 1) { return null; } else { return this.questionnaireTypes[i + 1].id; }
   }
-  
+
 }
 
-export type QuestionnaireType = {id: string; label: string; personalMatchTarget: string; slug: string};
+export interface QuestionnaireType { id: string; label: string; personalMatchTarget: string; slug: string; }
 
-export type QuestionnaireAnswer = {
+export interface QuestionnaireAnswer {
   _id: string;
   item_text: string;
   active?: boolean;
@@ -112,7 +110,7 @@ export type QuestionnaireAnswer = {
   subansData?: QuestionnaireAnswer[];
 }
 
-export type Questionnaire = {
+export interface Questionnaire {
   _id: string;
   answers: QuestionnaireAnswer[];
   slug: string;
--- src/app/models/questionnaire.ts
+++ src/app/models/questionnaire.ts
@@ -7,12 +7,12 @@ export class Questionnaire {
   private _questionSP: string;
   private _choiceType: 'multiple' | string;
 
-  get id(){ return this._id; }
-  get answers(){ return this._answers; };
-  get slug(){ return this._slug; }
-  get choiceType(){ return this._choiceType; }
+  get id() { return this._id; }
+  get answers() { return this._answers; }
+  get slug() { return this._slug; }
+  get choiceType() { return this._choiceType; }
 
-  constructor(o: any){
+  constructor(o: any) {
     this._id = o._id;
     this._slug = o.slug || null;
     this._questionType = o.question_type || null;
@@ -20,17 +20,17 @@ export class Questionnaire {
     this._questionSP = o.sp_question || null;
     this._choiceType = o.choice_type || 'multiple';
 
-    if(o.answers){
+    if (o.answers) {
       this._answers = [];
-      o.answers.forEach(ans=>{
+      o.answers.forEach(ans => {
         const a = new Answer(ans);
-        if(ans.subans){ a.setHasChild(true); }
+        if (ans.subans) { a.setHasChild(true); }
         this._answers.push(a);
-      })
+      });
     }
   }
 
-  setAnswers(answers: Answer[]){ this._answers = answers; }
+  setAnswers(answers: Answer[]) { this._answers = answers; }
 }
 
 export class Answer {
@@ -41,17 +41,17 @@ export class Answer {
   private _subAnswers: Answer[];
   private _level: number;
 
-  get id(){ return this._id; }
-  get itemText(){ return this._itemText; }
+  get id() { return this._id; }
+  get itemText() { return this._itemText; }
 
-  get hasChild(){ return this._hasChild; }
-  get subAnswers(){ return this._subAnswers; }
+  get hasChild() { return this._hasChild; }
+  get subAnswers() { return this._subAnswers; }
 
-  get isActive(){ return this._isActive; }
-  get isRoot(){ return (this._level == 0); }
-  get isChild(){ return (this._level == 1); }
+  get isActive() { return this._isActive; }
+  get isRoot() { return (this._level === 0); }
+  get isChild() { return (this._level === 1); }
 
-  constructor(o: any){
+  constructor(o: any) {
     this._id = o._id;
     this._itemText = o.item_text;
     this._isActive = false;
@@ -60,15 +60,15 @@ export class Answer {
     this._hasChild = false;
   }
 
-  activate(isActive: boolean = true){ this._isActive = isActive; }
+  activate(isActive: boolean = true) { this._isActive = isActive; }
 
-  setAsChild(){ this._level = 1; }
-  setHasChild(hasChild: boolean){ this._hasChild = hasChild; }
+  setAsChild() { this._level = 1; }
+  setHasChild(hasChild: boolean) { this._hasChild = hasChild; }
 
-  setSubAnswers(aSubs: any[]){
+  setSubAnswers(aSubs: any[]) {
     this._hasChild = true;
     this._subAnswers = [];
-    aSubs.forEach(aSub=>{
+    aSubs.forEach(aSub => {
       const a = new Answer(aSub);
       a.setAsChild();
       this._subAnswers.push(a);

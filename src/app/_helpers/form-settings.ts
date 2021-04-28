3492e1ab638c592e1ddc33b8ca29d9773fe77e2f
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-04-27 21:00:06 -0700

    bug fixed - add validator for selecting address

--- src/app/_helpers/form-settings.ts
+++ src/app/_helpers/form-settings.ts
@@ -1,4 +1,4 @@
-import { FormArray, ValidatorFn, Validators } from "@angular/forms"
+import { FormArray, FormControl, FormGroup, ValidatorFn, Validators } from "@angular/forms"
 
 export const minmax = {
   nameMax: 100,
@@ -36,12 +36,50 @@ const validatorCheckboxSelectedAtLeast = (minRequired: number = 1): ValidatorFn 
   }
 }
 
+const validatorNestedCheckboxSelectedAtLeast = (minRequired: number = 1): ValidatorFn => {
+  return function validate(formGroup: FormGroup) {
+    let checked = 0;
+    if(formGroup.value.root && formGroup.value.root.length > 0){
+      formGroup.value.root.forEach((isChecked: boolean) => {
+        if(isChecked){
+          checked ++;
+        }
+      });
+    }
+    
+    if(checked >= minRequired){
+      return null;
+    }else{
+      const errors = {}
+      errors['requiredCheckboxSelectedAtLeast' + minRequired] = true;
+      return errors;
+    }
+  }
+}
+
+const validatorAddressSelectedFromSuggestion = (): ValidatorFn => {
+  return function validate(formGroup: FormGroup) {
+    const lat = formGroup.controls.latitude.value;
+    const lng = formGroup.controls.longitude.value;
+
+    if(!formGroup.controls.address.value) {
+      return null;
+    }else if(lat != 0 && lng != 0){
+      return null;
+    }else{
+      const errors = {'addressSelectedFromSuggestion': true};
+      return errors;
+    }
+  }
+}
+
 const validatorFirstNameClient = [Validators.maxLength(minmax.nameMax), Validators.required];
 const validatorLastNameClient = [Validators.maxLength(minmax.nameMax)];
 const validatorNameSP = [Validators.required, Validators.minLength(3), Validators.maxLength(minmax.nameMax)];
 const validatorEmail = [Validators.required, Validators.email];
 const validatorPhone = [Validators.pattern(pattern.phone), Validators.minLength(minmax.phoneMin), Validators.maxLength(minmax.phoneMax)];
 const validatorRequired = [Validators.required];
+const validatorRequiredTrue = [Validators.requiredTrue];
 const validatorUrl = [Validators.pattern(pattern.url)];
 const validatorProfessionalTitle = [Validators.maxLength(minmax.professionalTitleMax)];
 const validatorProfessionalOrganization = [Validators.maxLength(minmax.professionalOrganizationMax)];
@@ -50,8 +88,8 @@ const validatorExactPricing = [Validators.pattern(pattern.price)];
 const validatorExactPricingRequired = [Validators.pattern(pattern.price), Validators.required];
 const validatorTextarea = [Validators.maxLength(minmax.textareaMax)];
 
-
 export const validators = {
+  profileImageProvider: validatorRequired,
   nameCentre: validatorNameSP,
   nameProvider: validatorNameSP,
   namePartner: validatorNameSP,
@@ -61,6 +99,7 @@ export const validators = {
   phone: validatorPhone,
   gender: validatorRequired,
   address: validatorRequired,
+  addressSelectedFromSuggestion: validatorAddressSelectedFromSuggestion(),
   addressClient: [],
   website: validatorUrl,
   bookingURL: validatorUrl,
@@ -69,6 +108,10 @@ export const validators = {
   certification: validatorCertification,
   ageRange: validatorCheckboxSelectedAtLeast(1),
   typicalHours: validatorCheckboxSelectedAtLeast(1),
+  customerHealth: validatorNestedCheckboxSelectedAtLeast(1),
+  typeOfProvider: [],
+  treatmentModality: [],
+  service: validatorNestedCheckboxSelectedAtLeast(1),
   exactPricing: validatorExactPricing,
   exactPricingRequired: validatorExactPricingRequired,
   businessKind: validatorRequired,
@@ -76,4 +119,7 @@ export const validators = {
 
   personalMatchGender: validatorRequired,
   personalMatchAgeRange: validatorRequired,
+  
+  accredit: validatorRequiredTrue,
+  
 }

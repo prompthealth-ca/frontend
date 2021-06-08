3e6820c6502c57718c3a133855448b1994807535
takayuki <takayuki.hiraishi@gmail.com>
2021-06-07 18:57:29 -0700

    complete page updated

--- src/app/dashboard/register-questionnaire-complete/register-questionnaire-complete.component.ts
+++ src/app/dashboard/register-questionnaire-complete/register-questionnaire-complete.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { IUserDetail } from 'src/app/models/user-detail';
 import { UniversalService } from 'src/app/shared/services/universal.service';
-import { RegisterType } from '../register-questionnaire/register-questionnaire.component';
 
 @Component({
   selector: 'app-register-questionnaire-complete',
@@ -12,39 +11,26 @@ import { RegisterType } from '../register-questionnaire/register-questionnaire.c
 export class RegisterQuestionnaireCompleteComponent implements OnInit {
 
   constructor(
-    private _toastr: ToastrService,
     private _router: Router,
-    private _route: ActivatedRoute,
     private _uService: UniversalService,
   ) { }
 
   public routePlan: string[];
-  private timer: any;
 
   ngOnInit(): void {
     this._uService.setMeta(this._router.url, {
       title: 'Registration complete | PromptHealth',
     });
-    this._route.queryParams.subscribe((params: {type: RegisterType}) => {
-      // let message: string;
 
-      this.routePlan = (params.type == 'practitioner') ? ['/plans'] : ['/plans/product'];
-      // message = 'You will be redirected to plan page in 5 seconds.';
-
-
-      // if(this.backToAmbassador) {
-      //   this.nextRoute = ['/ambassador-program'];
-      //   message = 'You will be redirected to Ambassador program page in 5 seconds.';
-      // }else{
-      // }
-
-      // this._toastr.success(message);
-
-      // this.timer = setTimeout(()=>{
-      //   this._router.navigate(this.nextRoute);
-      // }, 5000);
-  
-    });
+    const routePlan = ['/plans'];
+    const userStr = this._uService.localStorage.getItem('user');
+    if(userStr) {
+      const user: IUserDetail = JSON.parse(userStr);
+      if(user.roles == 'P') {
+        routePlan.push('product');
+      }
+    }
+    this.routePlan = routePlan;
   }
 }
 

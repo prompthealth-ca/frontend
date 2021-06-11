d488a27f1b272122f705352955bc4b114581987b
takayuki <takayuki.hiraishi@gmail.com>
2021-06-10 18:49:13 -0700

    category on home updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -43,54 +43,23 @@ export class HomeComponent implements OnInit {
     public countPractitionersFeaturedPerPage: number = 7;
     public citiesFeatured: {id: CityId, label: string}[];
 
-    categoryIcon(cat: Category): string {
-      const img = cat.image;
-      const img2 = img.toLowerCase().replace(/_/g, '-').replace('.png', '');
-      return img2
-    }
-    subCategoriesString(parent: Category) {
-      const categories = [];
-      parent.subCategory.forEach(sub => {categories.push(sub.item_text); });
-      return categories.join(' / ');
-    }
-
-    public categories: Category[];
-    public idxTargetSubcategoryInjector: number = null;
-    public idxSelectedCategory: number = null;
-    private _timerSubcategory: any = null    
-
-    onTapMainCategory(i: number) {
-      /** if main category is selected and same main category is clicked, hide subcategory */
-      if(this.idxSelectedCategory === i) {
-        this.hideSubcategory();
+    private timerCategory: any = null;
+    @HostListener('window:resize', ['$event']) WindowResize(e: Event) {
+      this.categoryController.disposeAll();
+      if(this.timerCategory) {
+        clearTimeout(this.timerCategory);
       }
-      /** if no main category is selected, show subcategory */
-      else if (this.idxSelectedCategory === null) {
-        this.showSubcategory(i);
-      }
-      /** if main category is selected and another main category is clicked, hide current subcategory and then show new subcategory */
-      else {
-        clearTimeout(this._timerSubcategory);
 
-        this.hideSubcategory();
-        this._timerSubcategory = setTimeout(() => {
-          this.showSubcategory(i);
-          this._changeDetector.detectChanges();            
-        }, 300);
-      }
+      this.timerCategory = setTimeout(() => {
+        if(this.categories) {
+          this.categoryController = new CategoryViewerController(this.categories);
+          this._changeDetector.detectChanges();
+        }
+      }, 500); 
     }
 
-    hideSubcategory() {
-      this.idxSelectedCategory = null;
-      this.idxTargetSubcategoryInjector = null;
-    }
-
-    showSubcategory(i: number) {
-      this.idxSelectedCategory = i;
-      const w = window.innerWidth || 320;
-      const colnum = (w < 768) ? 1 : (w < 1200) ? 2 : 3;
-      this.idxTargetSubcategoryInjector = (Math.floor(i / colnum) + 1) * colnum - 1;
-    }
+    private categories: Category[];
+    public categoryController: CategoryViewerController;
 
     changeHeaderShadowStatus(isShown: boolean) {
       if(isShown) {
@@ -118,6 +87,7 @@ export class HomeComponent implements OnInit {
 
     this._catService.getCategoryAsync().then((cats => {
       this.categories = cats;
+      this.categoryController = new CategoryViewerController(this.categories);
     }));
 
     const cityIdsFeatured: CityId[] = ['toronto', 'vancouver', 'victoria', 'hamilton', 'richmond', 'burnaby', 'calgary', 'winnipeg'];
@@ -158,5 +128,100 @@ export class HomeComponent implements OnInit {
         this.practitionersFeatured = users;
       }
     }, (error) => { console.log(error); });
+  }
+}
+
+class CategoryViewerController {
+
+  public dataPerRows: {
+    categories: Category[];
+    categoryInjected: Category;
+  }[];
+
+  isCategorySelected(idxRow: number, idxCol: number) {
+    const target = this.dataPerRows[idxRow].categories[idxCol];
+    const injected = this.dataPerRows[idxRow].categoryInjected;
+    return (injected && target._id == injected._id) ? true : false;
+  }
+
+  getIconOf(cat: Category): string {
+    const img = cat.image;
+    const img2 = img.toLowerCase().replace(/_/g, '-').replace('.png', '');
+    return img2
+  }
+
+  subCategoriesString(parent: Category) {
+    const categories = [];
+    parent.subCategory.forEach(sub => {categories.push(sub.item_text); });
+    return categories.join(' / ');
+  }
+
+  onTapMainCategory(idxRow: number, idxCol: number) {
+    const data = this.dataPerRows[idxRow];
+
+    if(this.isCategorySelected(idxRow, idxCol)) { this._dispose(data); }
+    else {
+      const injected = data.categoryInjected;
+      const target = data.categories[idxCol];
+
+      if(!injected) { 
+        this._inject(data, target); 
+      }
+      else {
+        this._dispose(data);
+        this.waitForInject = {idxRow: idxRow, idxCol: idxCol};
+      }
+    }
+
+    this.isCategorySelected(idxRow, idxCol)
+  }
+
+  onAnimationDone() {
+    if(this.waitForInject) {
+      const data = this.dataPerRows[this.waitForInject.idxRow];
+      const target = data.categories[this.waitForInject.idxCol];
+      this._inject(data, target);
+      this.waitForInject = null;
+    }
+  }
+
+  disposeAll() {
+    this.dataPerRows.forEach(data => { this._dispose(data); });
+  }
+
+  private waitForInject: {idxRow: number; idxCol: number} = null;
+
+  private _inject(dataPerRow: CategoryViewerController['dataPerRows'][0], target: Category) {
+    dataPerRow.categoryInjected = target;
+  }
+
+  private _dispose(dataPerRow: CategoryViewerController['dataPerRows'][0]) {
+    dataPerRow.categoryInjected = null;
+  }
+
+
+  constructor(categories: Category[]) {
+    this.dataPerRows = [];
+
+    let numcol: number;
+    if(!window.innerWidth || window.innerWidth < 768) { numcol = 1; }
+    else if(window.innerWidth < 1200) { numcol = 2} 
+    else { numcol = 3; }
+
+    const numrow = Math.ceil(categories.length / numcol);
+
+    for (let i=0; i<numrow; i++) {
+      const categoriesPerRow = [];
+      for (let j=0; j<numcol; j++) {
+        let k = i * numcol + j;
+        if(k >= categories.length) { break; }
+
+        categoriesPerRow.push(categories[k]);
+      }
+
+      if(categoriesPerRow.length == 0) { break; }
+      
+      this.dataPerRows.push({categories: categoriesPerRow, categoryInjected: null});
+    }
   }
 }

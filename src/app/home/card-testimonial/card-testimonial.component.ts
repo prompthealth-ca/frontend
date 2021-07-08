33f57996a16648e1328150501574339d8595ffa8
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-07-08 15:42:45 -0700

    card testimonial added

---
+++ src/app/home/card-testimonial/card-testimonial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-testimonial',
+  templateUrl: './card-testimonial.component.html',
+  styleUrls: ['./card-testimonial.component.scss']
+})
+export class CardTestimonialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

7d71cbeb32801a32026548dc22623cda20db40b8
takayuki <takayuki.hiraishi@gmail.com>
2021-07-15 17:19:41 -0700

    social updated

---
+++ src/app/social/card-new-post/card-new-post.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-new-post',
+  templateUrl: './card-new-post.component.html',
+  styleUrls: ['./card-new-post.component.scss']
+})
+export class CardNewPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}

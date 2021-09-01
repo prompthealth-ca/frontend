33f84c952aab84459e6c5e8b4e82070dc50e1197
takayuki <takayuki.hiraishi@gmail.com>
2021-09-01 14:21:34 -0700

    bell feature added small changes

--- src/app/social/card-new-post/card-new-post.component.ts
+++ src/app/social/card-new-post/card-new-post.component.ts
@@ -298,6 +298,7 @@ export class CardNewPostComponent implements OnInit {
         this.formItemService.deselectAll();
         this.onPublished.emit(res.data as ISocialPost);
         this._editorService.resetForm();
+        this.imagePreview = null;
       } else {
         console.log(res.message);
         this._toastr.error('Could not upload note. Please try again later.')

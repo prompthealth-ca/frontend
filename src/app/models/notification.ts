f8c3143585c9f131a567f1629ae9ff5187772977
takayuki_hiraishi <takayuki.hiraishi@gmail.com>
2021-09-01 08:22:09 -0700

    add link on notification add NoName for no name user on follow list

--- src/app/models/notification.ts
+++ src/app/models/notification.ts
@@ -9,6 +9,7 @@ export interface ISocialNotification {
   isRead?: boolean;
   image?: string;
   link?: string;
+  type?: 'following' | 'new-event' | 'new-article' | 'password-change';
   iOSLink?: string;
   androidLink?: string;
   createdAt?: string | Date;
@@ -27,9 +28,23 @@ export class SocialNotification implements ISocialNotification{
   get link() { return this.data.link; }
   get createdAt() { return this.data.createdAt; }
 
+  get linkToTarget() {
+    let link = '';
+    if(this.data.link) {
+      switch(this.data.type) {
+        case 'following': link = '/community/profile/' + this.data.link; break;
+        case 'new-article':
+        case 'new-event': link = '/community/content/' + this.data.link; break;
+      }      
+    }
+    return link;
+  }
+
   private _s3 = environment.config.AWS_S3;
 
-  constructor(private data: ISocialNotification) {}
+  constructor(private data: ISocialNotification) {
+    console.log(data);
+  }
 
   markAsRead() {
     this.data.isRead = true;

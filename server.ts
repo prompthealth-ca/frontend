b5d72e67c29d684781dbdf149b5abcef94ddd3bc
takayuki <takayuki.hiraishi@gmail.com>
2021-09-22 11:39:36 -0700

    build updated

--- server.ts
+++ server.ts
@@ -69,6 +69,7 @@ export function app() {
 
   /** api proxy */
   const apiProxy = proxy('/api', {target: environment.config.BACKEND_BASE, changeOrigin: false});
+  server.use('/api', apiProxy)
 
   /** client side rendering */
   server.use('/auth',                  (req, res) => { res.sendFile(join(distFolder, 'index.html')); })

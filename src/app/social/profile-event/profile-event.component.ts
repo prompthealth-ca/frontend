548e95830baaf2128bd97aa6ec95d9861cae5c21
takayuki <takayuki.hiraishi@gmail.com>
2021-11-22 20:50:41 -0800

    show past event in profile event tab Fixes #89

---
+++ src/app/social/profile-event/profile-event.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-profile-event',
+  templateUrl: './profile-event.component.html',
+  styleUrls: ['./profile-event.component.scss'],
+})
+export class ProfileEventComponent implements OnInit {
+
+  constructor( ) { }
+
+  ngOnInit() {}
+}
